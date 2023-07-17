6eb5a15cf80145657552e011a53d600cdad49f19
hyejun <solar3070@naver.com>
2023-07-17 19:24:27 +0900

    style: COLOR 상수 선언

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+const COLOR = {
+  white: "#fff",
+  black: "#000",
+  gray: "#989CAC",
+  purple: "#7F7CF7",
+  dark100: "#43464F",
+  dark200: "#333740",
+  dark300: "#2B2D34",
+};
+
+export default COLOR;
