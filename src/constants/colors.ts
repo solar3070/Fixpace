9f5c3e71a4a07dfa7f9691ee30c1ce451fdc9aa3
hyejun <solar3070@naver.com>
2023-07-18 02:15:40 +0900

    style: 컬러 추가

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -3,6 +3,7 @@ const COLOR = {
   black: "#000",
   gray: "#989CAC",
   purple: "#7F7CF7",
+  purpleOpacity: "rgba(127, 124, 247, 0.2)",
   dark100: "#43464F",
   dark200: "#333740",
   dark300: "#2B2D34",
