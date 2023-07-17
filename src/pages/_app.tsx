5b22d370fd442d663c76c08b909936df697f4209
hyejun <solar3070@naver.com>
2023-07-17 19:23:05 +0900

    style: 글로벌 스타일 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
+import GlobalStyle from "@/styles/GlobalStyle";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
