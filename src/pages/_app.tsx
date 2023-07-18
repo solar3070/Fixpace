7a9f039ef789cfa2be3e36dbc42990f4477ae504
Hyejun Lee <solar3070@naver.com>
2023-07-18 13:36:46 +0900

    타이틀 및 파비콘 설정 (#9)
    * asset: 파비콘 이미지 추가
    
    * chore: 타이틀 및 파비콘 지정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import GlobalStyle from "@/styles/GlobalStyle";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
+      <Head>
+        <title>Fixpace | 띄어쓰기 연습 공간</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <meta name="description" content="AI 기반의 한글 띄어쓰기 연습 서비스" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" sizes="32x32" />
+      </Head>
       <body>
         <Main />
         <NextScript />
