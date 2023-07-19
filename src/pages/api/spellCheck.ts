3b2489115cbff4c7d5dded73db936f6b75ea243c
solar3070 <>
2023-07-19 16:06:02 +0900

    feat: 맞춤법 검사 api route 설정

---
+++ src/pages/api/spellCheck.ts
@@ -0,0 +1,33 @@
+import type { ResponseError, SpellCheck } from "@/types";
+import { isAxiosError } from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const hanspell = require("hanspell");
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<SpellCheck[] | ResponseError>) {
+  const text = req.body.text || "";
+
+  const end = function () {
+    console.log("맞춤법 검사 완료");
+  };
+
+  const error = function (err: string) {
+    console.error("맞춤법 검사 에러 발생 " + err);
+  };
+
+  const result = (data: SpellCheck[]) => {
+    res.status(200).json(data);
+  };
+
+  try {
+    hanspell.spellCheckByPNU(text, 6000, result, end, error);
+  } catch (error) {
+    if (isAxiosError<ResponseError>(error)) {
+      if (error.response) {
+        res.status(error.response.status).json(error.response.data);
+      } else {
+        res.status(500).json({ message: "An error occurred during your request." });
+      }
+    }
+  }
+}
