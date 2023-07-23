60145e137bc3ed48af9b4d5fad68e87737ebbefa
solar3070 <>
2023-07-23 19:27:43 +0900

    feat: 사용자 새로고침 클릭시 전역 상태 초기화

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import { Layout } from "@/components/common";
 import Keyword from "@/components/Keyword";
 import Typing from "@/components/Typing";
-import { FormEvent, useState } from "react";
+import { correctTextState } from "@/recoil/atom";
+import { FormEvent, useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
 
 export default function Home() {
   const [keyword, setKeyword] = useState("");
   const [submitted, setSubmitted] = useState(false);
+
+  const [correctText, setCorrectText] = useRecoilState(correctTextState);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>, keyword: string) => {
     e.preventDefault();
@@ -13,10 +17,16 @@ export default function Home() {
     setSubmitted(true);
   };
 
+  useEffect(() => {
+    if (!submitted && correctText.length) {
+      setCorrectText([]);
+    }
+  }, []);
+
   return (
     <Layout>
       {!submitted && <Keyword onSubmit={handleSubmit} />}
-      {submitted && <Typing keyword={keyword} setSubmitted={setSubmitted}/>}
+      {submitted && <Typing keyword={keyword} setSubmitted={setSubmitted} />}
     </Layout>
   );
 }
