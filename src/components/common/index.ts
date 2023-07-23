9a49b5f38db883113841448391da642bb83c898f
solar3070 <>
2023-07-23 18:55:51 +0900

    chore: 공통 컴포넌트 index에 스켈레톤 추가

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header";
 export { default as Input } from "./Input";
 export { default as Layout } from "./Layout";
+export { default as Skeleton } from "./Skeleton";
