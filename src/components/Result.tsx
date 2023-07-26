5e3c5a078e155f9e52e24f6b07439f609983d5cb
solar3070 <>
2023-07-26 10:14:55 +0900

    feat: 마지막 제출시 폭죽 효과 적용

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -5,6 +5,7 @@ import { correctTextState } from "@/recoil/atom";
 import { StepType } from "@/types";
 import { calcAccuracy, checkUserInput } from "@/utils";
 import styled from "@emotion/styled";
+import ConfettiExplosion, { ConfettiProps } from "react-confetti-explosion";
 import { useRecoilValue } from "recoil";
 
 interface ResultProps {
@@ -16,8 +17,19 @@ function Result({ userInputList, changeStep }: ResultProps) {
   let accuracySum = 0;
   const correctList = useRecoilValue(correctTextState);
 
+  const largeProps: ConfettiProps = {
+    force: 0.8,
+    duration: 3000,
+    particleCount: 200,
+    width: 1300,
+    colors: [COLOR.purple, COLOR.white, COLOR.dark200],
+  };
+
   return (
     <>
+      <StParticle>
+        <ConfettiExplosion {...largeProps} />
+      </StParticle>
       <StTextWrapper>
         {userInputList.map((userInput, index) => {
           const result = checkUserInput(userInput, correctList[index]);
@@ -33,6 +45,11 @@ function Result({ userInputList, changeStep }: ResultProps) {
 }
 
 export default Result;
+
+const StParticle = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 const StTextWrapper = styled.div`
   width: 100%;
