06b99a74481035bc7b88e0dd7dc95b0456bdf59a
Hyejun Lee <solar3070@naver.com>
2023-07-26 18:49:22 +0900

    버그 수정 (#36)
    * fix: 전역 상태 초기화 안되는 오류 수정
    
    * feat: 환경변수 추가
    
    ---------
    
    Co-authored-by: solar3070 <>

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,12 +1,14 @@
 import type { SpellCheck, Text } from "@/types";
 import axios from "axios";
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
 export const generateTextByOpenAI = async (keyword: string) => {
-  const response = await axios.post<Text>("/api/generateText", { keyword });
+  const response = await axios.post<Text>(`${BASE_URL}/api/generateText`, { keyword });
   return response.data;
 };
 
 export const spellCheckText = async (text: string) => {
-  const response = await axios.post<SpellCheck[]>("/api/spellCheck", { text });
+  const response = await axios.post<SpellCheck[]>(`${BASE_URL}/api/spellCheck`, { text });
   return response.data;
 };
