73ff8924ec3c932af48a617ed1c8e0c9067888c9
solar3070 <>
2023-07-29 16:30:36 +0900

    feat: 에러 메세지 생성 함수 정의

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ export { default as animateSpace } from "./animateSpace";
 export { default as calcAccuracy } from "./calcAccuracy";
 export { default as checkText } from "./checkText";
 export { default as checkUserInput } from "./checkUserInput";
+export { default as createErrorMessage } from "./createErrorMessage";
 export { default as removeSpace } from "./removeSpace";
 export { default as splitText } from "./splitText";
 export { default as validateInput } from "./validateInput";
