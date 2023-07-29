e17251d1abed5fa98d472516ea4e51e69626346c
solar3070 <>
2023-07-29 16:31:05 +0900

    feat: 맞춤법 검사 에러 핸들링

--- src/pages/api/spellCheck.ts
+++ src/pages/api/spellCheck.ts
@@ -1,5 +1,4 @@
 import type { ResponseError, SpellCheck } from "@/types";
-import { isAxiosError } from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const hanspell = require("hanspell");
@@ -7,27 +6,31 @@ const hanspell = require("hanspell");
 export default async function handler(req: NextApiRequest, res: NextApiResponse<SpellCheck[] | ResponseError>) {
   const text = req.body.text;
 
-  const end = function () {
-    console.log("맞춤법 검사 완료");
+  const endDAUM = function () {
+    console.log("다음 맞춤법 검사기 검사 완료");
   };
 
-  const error = function (err: string) {
-    console.error("맞춤법 검사 에러 발생 " + err);
-  };
-
-  const result = (data: SpellCheck[]) => {
+  const CheckResultByDAUM = (data: SpellCheck[]) => {
     res.status(200).json(data);
   };
 
-  try {
-    hanspell.spellCheckByPNU(text, 6000, result, end, error);
-  } catch (error) {
-    if (isAxiosError<ResponseError>(error)) {
-      if (error.response) {
-        res.status(error.response.status).json(error.response.data);
-      } else {
-        res.status(500).json({ message: "맞춤법 검사 요청 중에 에러가 발생했습니다." });
-      }
-    }
-  }
+  const CheckErrorByDAUM = function (err: Error) {
+    console.error("다음 맞춤법 검사 에러 발생: " + err);
+    res.status(200).json([]);
+  };
+
+  const endPNU = function () {
+    console.log("부산대 맞춤법 검사기 검사 완료");
+  };
+
+  const CheckResultByPNU = (data: SpellCheck[]) => {
+    res.status(200).json(data);
+  };
+
+  const CheckErrorByPNU = function (err: Error) {
+    console.error("부산대 맞춤법 검사 에러 발생: " + err);
+    hanspell.spellCheckByDAUM(text, 6000, CheckResultByDAUM, endDAUM, CheckErrorByDAUM);
+  };
+
+  hanspell.spellCheckByPNU(text, 6000, CheckResultByPNU, endPNU, CheckErrorByPNU);
 }
